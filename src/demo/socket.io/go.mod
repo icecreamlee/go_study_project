057298cdfd336e842fff71d573b06a9f82d746b1
Icecream <981464828@qq.com>
2019-10-25 19:08:20 +0800

    normal commit

---
+++ src/demo/socket.io/go.mod
@@ -0,0 +1,8 @@
+module demo/socket.io
+
+go 1.13
+
+require (
+	github.com/googollee/go-socket.io v1.4.2 // indirect
+	github.com/rs/cors v1.7.0 // indirect
+)
--- src/demo/socket.io/main.go
+++ src/demo/socket.io/main.go
@@ -1,65 +1,87 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-    "github.com/googollee/go-socket.io"
+	"github.com/googollee/go-engine.io"
+	"github.com/googollee/go-engine.io/transport"
+	"github.com/googollee/go-engine.io/transport/polling"
+	"github.com/googollee/go-engine.io/transport/websocket"
+	socketio "github.com/googollee/go-socket.io"
+	"github.com/rs/cors"
 )
 
 var server *socketio.Server
 
 func main() {
-    var err error
-    server, err = socketio.NewServer(nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-    server.OnConnect("/", func(s socketio.Conn) error {
-        s.SetContext("")
-        fmt.Println("connected:", s.ID())
-        server.JoinRoom(s.ID(), s)
-        //s.Join(s.ID())
-        return nil
-    })
-    server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-        fmt.Println("notice:", msg)
-        s.Emit("reply", "have "+msg)
-    })
-    server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-        s.SetContext(msg)
-        return "recv " + msg
-    })
-    server.OnEvent("/", "bye", func(s socketio.Conn) string {
-        last := s.Context().(string)
-        s.Emit("bye", last)
-        err := s.Close()
-        if err != nil {
-            log.Fatal("close err: ", err)
-        }
-        return last
-    })
-    server.OnError("/", func(e error) {
-        fmt.Println("meet error:", e)
-    })
-    server.OnDisconnect("/", func(s socketio.Conn, msg string) {
-        fmt.Println("closed", msg)
-    })
-    go server.Serve()
-    defer server.Close()
+	pt := polling.Default
+	wt := websocket.Default
+	wt.CheckOrigin = func(req *http.Request) bool {
+		return true
+	}
 
-    http.Handle("/socket.io/", server)
-    http.Handle("/", http.FileServer(http.Dir("./asset")))
-    log.Println("Serving at localhost:8000...")
-    http.HandleFunc("/p", sayhelloName)
-    log.Fatal(http.ListenAndServe(":8000", nil))
+	var err error
+	server, err = socketio.NewServer(&engineio.Options{
+		Transports: []transport.Transport{
+			pt,
+			wt,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+
+	server.OnEvent("/", "login", func(s socketio.Conn, msg string) {
+		s.Join(msg)
+		fmt.Println("login: ", msg)
+	})
+	server.OnEvent("/", "bye", func(s socketio.Conn) string {
+		last := s.Context().(string)
+		s.Emit("bye", last)
+		s.Close()
+		return last
+	})
+	server.OnError("/", func(e error) {
+		fmt.Println("meet error:", e)
+	})
+	server.OnDisconnect("/", func(s socketio.Conn, msg string) {
+		fmt.Println("closed", msg)
+	})
+
+	go server.Serve()
+	defer server.Close()
+
+	mux := http.NewServeMux()
+	mux.Handle("/socket.io/", server)
+	mux.HandleFunc("/p", sayhelloName)
+	//http.Handle("/", http.FileServer(http.Dir("./asset")))
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://dev.yichefu.cn", "https://dev.yichefu.cn"},
+		AllowedMethods:   []string{"GET", "PUT", "OPTIONS", "POST", "DELETE"},
+		AllowCredentials: true,
+	})
+
+	// decorate existing handler with cors functionality set in c
+	handler := c.Handler(mux)
+
+	log.Println("Serving at localhost:8000...")
+	log.Fatal(http.ListenAndServeTLS(":8000", "fullchain.crt", "private.pem", handler))
 }
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    //server.BroadcastToRoom(r.Form.Get("id"), "reply", "Broadcast")
-    server.BroadcastToRoom(r.Form.Get("id"), "reply", "Broadcast", func (so socketio.Server, data string) {
-        log.Println("Client ACK with data: ", data)
-    })
-    fmt.Fprintf(w, "BroadcastToRoom") //这个写入到w的是输出到客户端的
+	//server.BroadcastToRoom(r.Form.Get("id"), "reply", "Broadcast")
+	_ = r.ParseForm()
+	server.BroadcastToRoom(r.Form.Get("id"), "message", "Broadcast", func(so socketio.Server, data string) {
+		log.Println("Client ACK with data: ", data)
+	})
+	fmt.Fprintf(w, "BroadcastToRoom") //这个写入到w的是输出到客户端的
 }
